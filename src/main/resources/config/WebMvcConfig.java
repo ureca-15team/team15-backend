057cb2d75973f64583fa2f532bb42570677f50bf
99JIWONDEV <99jiwonpark@gmail.com>
2025-03-21 09:16:58 +0900

    fix: EB 새로고침 에러  해결 - 라우터변경

--- src/main/resources/config/WebMvcConfig.java
+++ src/main/resources/config/WebMvcConfig.java
@@ -1,17 +1,19 @@
-package com.shop.living.config;
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
         registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css|\\.ico|\\.json)$}")
+                .setViewName("forward:/index.html");
     }
 
     @Override
